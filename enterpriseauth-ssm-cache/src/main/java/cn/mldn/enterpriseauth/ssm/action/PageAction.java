dc226d5c4ab4046a51e2159a58cd73bc0e130b75
mldn <mldnqa@163.com>
2017-11-12 13:12:18 +0800

    创建enterpriseauth-ssm-cache的项目模块，讲解缓存处理操作。

---
+++ enterpriseauth-ssm-cache/src/main/java/cn/mldn/enterpriseauth/ssm/action/PageAction.java
@@ -0,0 +1,29 @@
+package cn.mldn.enterpriseauth.ssm.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PageAction {
+	@RequestMapping("/loginForm")
+	public String loginForm() {
+		return "login";
+	}
+
+	@RequestMapping("/pages/back/welcome")
+	public String welcome() {
+		return "back/welcome";
+	}
+
+	@RequestMapping("/logout")
+	public String logout() {
+		return "login";
+	}
+
+	@ResponseBody
+	@RequestMapping("/unauth")
+	public Object unauth() {
+		return "【ERROR - AUTH】没有此类处理权限。";
+	}
+}
